fe3c7ce0e169d0228df3c7a63a3630b62cf0b1d7
rioka <rioka@libero.it>
2023-06-08 16:28:39 +0100

    - Kill most projects - Rename `NsbRouterPlayground.Integration.Messages.csproj`

---
+++ NsbRouterPlayground.Messages/Events/VendorCreated.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace NsbRouterPlayground.Integration.Messages.Events {
+
+   public class VendorCreated {
+
+      public Guid Uid { get; set; }
+
+      public string Name { get; set; }
+   }
+}
