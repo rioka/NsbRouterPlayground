4ed8bcf1fd7c40c5396441747a79f35f1948a0d1
rioka <rioka@libero.it>
2023-06-08 16:54:11 +0100

    Add `NsbRouterPlayground.Sender`

---
+++ NsbRouterPlayground.Sender/Program.SendMessages.cs
@@ -0,0 +1,36 @@
+﻿using NsbRouterPlayground.Common.Messages.Commands;
+using NServiceBus;
+
+namespace NsbRouterPlayground.Sender;
+
+internal partial class Program
+{
+  private static async Task SendMessages(IMessageSession session)
+  {
+    var exit = false;
+    
+    while (!exit)
+    {
+      Console.WriteLine("Press '1' enter to create an order");
+      Console.WriteLine("Press any key to exit");
+      
+      var ch = Console.ReadKey();
+      Console.WriteLine();
+
+      switch (ch.Key)
+      {
+        case ConsoleKey.D1:
+        case ConsoleKey.NumPad1:
+          var createOrder = new CreateVendor() {
+            Id = Guid.NewGuid()
+          };
+          await session.Send(createOrder);
+          break;
+
+        default:
+          exit = true;
+          break;
+      }
+    }
+  }
+}
---
+++ NsbRouterPlayground.Sender/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NsbRouterPlayground.Bootstrap;
+using NsbRouterPlayground.Bootstrap.Infrastructure;
+using NsbRouterPlayground.Common;
+using NsbRouterPlayground.Common.Messages.Commands;
+using NServiceBus;
+
+namespace NsbRouterPlayground.Sender;
+
+internal partial class Program
+{
+  public static async Task Main(string[] args)
+  {
+    var host = CreateHostBuilder(args)
+      .Build();
+
+    using (host)
+    {
+      var config = host.Services.GetRequiredService<IConfiguration>();
+      await DbHelpers.EnsureDatabaseExists(config.GetConnectionString("Data"));
+      
+      await host.StartAsync();
+      var lifetime = host.Services.GetRequiredService<IHostApplicationLifetime>();
+      var session = host.Services.GetRequiredService<IMessageSession>();
+
+      await SendMessages(session); 
+
+      lifetime.StopApplication();
+      await host.WaitForShutdownAsync();
+    }
+  }
+
+  private static IHostBuilder CreateHostBuilder(string[] args)
+  {
+    var hb = Host
+      .CreateDefaultBuilder(args)
+      .UseConsoleLifetime()
+      .UseNServiceBus(ctx => {
+
+        var endpointConfig = Bootstrapper.Configure(Endpoints.Sender, ctx.Configuration.GetConnectionString("Data"), messages: new [] {
+          typeof(CreateVendor)
+        });
+        return endpointConfig;
+      });
+    
+    return hb;
+  }
+}
