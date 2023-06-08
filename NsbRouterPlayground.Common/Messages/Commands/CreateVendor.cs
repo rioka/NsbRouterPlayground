aaad3b2129c523d44e9e5c5eca4a6adfa92758d0
rioka <rioka@libero.it>
2023-06-08 17:31:26 +0100

    Formatting

--- NsbRouterPlayground.Common/Messages/Commands/CreateVendor.cs
+++ NsbRouterPlayground.Common/Messages/Commands/CreateVendor.cs
@@ -1,9 +1,9 @@
 using NsbRouterPlayground.Common.Attributes;
 
-namespace NsbRouterPlayground.Common.Messages.Commands; 
+namespace NsbRouterPlayground.Common.Messages.Commands;
 
 [NsbCommand(Endpoints.Receiver)]
-public class CreateVendor {
-
+public class CreateVendor
+{
   public Guid Id { get; set; }
 }
--- NsbRouterPlayground.Common/Messages/Events/VendorCreated.cs
+++ NsbRouterPlayground.Common/Messages/Events/VendorCreated.cs
@@ -1,10 +1,10 @@
 ﻿using NsbRouterPlayground.Common.Attributes;
 
-namespace NsbRouterPlayground.Common.Messages.Events; 
+namespace NsbRouterPlayground.Common.Messages.Events;
 
 [NsbEvent(Endpoints.Receiver)]
-public class VendorCreated {
-
+public class VendorCreated
+{
   public Guid Id { get; set; }
 
   public DateTime CreatedAt { get; set; }
